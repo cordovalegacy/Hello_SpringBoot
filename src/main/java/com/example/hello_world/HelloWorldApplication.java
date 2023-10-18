e2d572d77f873137b4a23984a18252946ec82e4f
Brendan Cordova <cordovalegacy19@gmail.com>
2023-10-17 19:44:38 -0700

    Created my first spring routes

--- src/main/java/com/example/hello_world/HelloWorldApplication.java
+++ src/main/java/com/example/hello_world/HelloWorldApplication.java
@@ -2,12 +2,26 @@ package com.example.hello_world;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
+@RestController
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		return "Hello World";
+	}
+	@RequestMapping("/newRoute")
+	public String newRoute() {
+		return "Hello Spring Boot";
 	}
 
 }
